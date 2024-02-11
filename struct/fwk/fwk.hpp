029c07d9e41b84c0394a92bf5b5bd287cd76166a
serna37 <serna37@serna37noMacBook-Pro.local>
2024-02-11 19:57:56 +0900

    add fenwick tree

---
+++ struct/fwk/fwk.hpp
@@ -0,0 +1,44 @@
+// TODO lazy update
+// TODO binary_search
+template <typename Monoid> struct FwkTree {
+    using M = Monoid;
+    using T = typename M::value_type;
+
+  private:
+    int N;
+    vector<T> node;
+    T tot;
+    void init() {
+        node.assign(N + 1, M::e);
+        tot = M::e;
+    }
+    void bind(T &x, const T &y) {
+        x = M::op(x, y);
+    }
+
+  public:
+    FwkTree(int n) : N(n) {
+        init();
+    }
+    FwkTree(const vector<T> &a) : N(a.size()) {
+        init();
+        for (int i = 1; i <= N; i++) {
+            bind(tot, a[i - 1]);
+            bind(node[i], a[i - 1]);
+            int f = i + (i & -i);
+            if (f <= N) bind(node[f], node[i]);
+        }
+    }
+    void act(int i, const T &x) {
+        tot += x;
+        for (++i; i <= N; i += i & -i) bind(node[i], x);
+    }
+    T get(int r) {
+        T ans = M::e;
+        for (++r; r; r -= r & -r) bind(ans, node[r]);
+        return ans;
+    }
+    T top() {
+        return tot;
+    }
+};
