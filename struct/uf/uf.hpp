ce77975c1173dbba420561adbe9416be3346bbd4
serna37 <serna37@serna37noMacBook-Pro.local>
2024-02-11 19:58:09 +0900

    add union find

---
+++ struct/uf/uf.hpp
@@ -0,0 +1,45 @@
+struct UnionFind {
+  private:
+    int N;
+    vector<int> dat;
+    void build() {
+        dat.assign(N, -1);
+    }
+
+  public:
+    UnionFind(int n = 0) : N(n) {
+        build();
+    }
+    void reset() {
+        build();
+    }
+    int operator[](int x) {
+        while (dat[x] >= 0) {
+            int pp = dat[dat[x]];
+            if (pp < 0) return dat[x];
+            x = dat[x] = pp;
+        }
+        return x;
+    }
+    long long size(int x) {
+        x = (*this)[x];
+        return -dat[x];
+    }
+    bool unite(int x, int y) {
+        x = (*this)[x], y = (*this)[y];
+        if (x == y) return false;
+        if (-dat[x] < -dat[y]) swap(x, y);
+        dat[x] += dat[y], dat[y] = x;
+        return true;
+    }
+    vector<vector<int>> groups() {
+        vector<vector<int>> ret(N);
+        for (int i = 0; i < N; i++) {
+            ret[(*this)[i]].emplace_back(i);
+        }
+        ret.erase(remove_if(begin(ret), end(ret),
+                            [&](const vector<int> &v) { return v.empty(); }),
+                  end(ret));
+        return ret;
+    }
+};
