e65104e6f76f25d2c334620c0403e704941745c2
serna37 <serna37@serna37noMacBook-Pro.local>
2024-02-12 09:17:24 +0900

    modify divisor loop

--- num/divisors.hpp
+++ num/divisors.hpp
@@ -1,6 +1,6 @@
 template <typename T> vector<T> divisors(T a) {
     vector<T> ans;
-    for (int i = 1; i * i <= a; i++) {
+    for (T i = 1; i * i <= a; i++) {
         if (a % i != 0) continue;
         ans.push_back(i);
         if (a / i != i) ans.push_back(a / i);
