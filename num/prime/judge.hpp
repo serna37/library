d9137b038eeb1ae9e6707d756cb09ee06d6a7427
serna37 <serna37@serna37noMacBook-Pro.local>
2024-02-13 22:19:07 +0900

    upd prime test

--- num/prime/judge.hpp
+++ num/prime/judge.hpp
@@ -1,9 +1,8 @@
-template <typename T> bool is_p(T num) {
-    if (num == 2) return true;
-    if (num < 2 || num % 2 == 0) return false;
-    double sqrtNum = sqrt(num);
-    for (T i = 3; i <= sqrtNum; i += 2) {
-        if (num % i == 0) return false;
+template <typename T> bool is_p(T N) {
+    if (N == 2) return true;
+    if (N == 1 or N % 2 == 0) return false;
+    for (T i = 3; i * i <= N; i += 2) {
+        if (N % i == 0) return false;
     }
     return true;
 }
