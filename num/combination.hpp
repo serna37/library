41338f19a883b4eff04d35f2d6d4709dfa493067
serna37 <serna37@serna37noMacBook-Pro.local>
2024-02-14 09:45:39 +0900

    add naive combination

---
+++ num/combination.hpp
@@ -0,0 +1,9 @@
+template <typename T> T nCk(int n, int k) {
+    if (k < 0 || n < k) return 0;
+    T ret = 1;
+    for (T i = 1; i <= k; ++i) {
+        ret *= n--;
+        ret /= i;
+    }
+    return ret;
+}
