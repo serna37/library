f8405406a57e629fe470bec01386f62f7fd08b36
serna37 <serna37@serna37noMacBook-Pro.local>
2024-02-17 16:21:51 +0900

    add edge construct

--- graph/base.hpp
+++ graph/base.hpp
@@ -2,5 +2,7 @@ template <typename T> struct Edge {
     int from, to;
     T cost;
     int id;
+    Edge(int from, int to, T cost = 1, int id = -1)
+        : from(from), to(to), cost(cost), id(id){};
 };
 template <typename T> using Graph = vector<vector<Edge<T>>>;
