b92effb5e79e32129af6c97f6f5723436cf3a886
serna37 <serna37@serna37noMacBook-Pro.local>
2024-02-19 12:49:56 +0900

    zip lambda -> function

--- seq/zip.hpp
+++ seq/zip.hpp
@@ -1,7 +1,7 @@
-auto zip = []<class T>(vector<T> a) -> pair<vector<T>, vector<T>> {
+template <typename T> pair<vector<T>, vector<T>> zip(vector<T> a) {
     vector<T> c = a;
     sort(c.begin(), c.end());
     c.erase(unique(c.begin(), c.end()), c.end());
     for (auto &v : a) v = lower_bound(c.begin(), c.end(), v) - c.begin();
     return {a, c};
-};
+}
