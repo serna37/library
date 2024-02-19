cf6162adbf4bdb7ab33d0d87bff6929ded2de7e8
serna37 <serna37@serna37noMacBook-Pro.local>
2024-02-19 10:20:01 +0900

    modify

--- seq/int_bisearch.hpp
+++
@@ -1,7 +0,0 @@
-template <typename T, typename F> T int_bisearch(T ok, T ng, const F &test) {
-    while (abs(ok - ng) > 1) {
-        T mid = (ok + ng) / 2;
-        (test(mid) ? ok : ng) = mid;
-    }
-    return ok;
-}
