6e1a3aabf4c3f47b802c3619d6060ba71c87245b
serna37 <serna37@serna37noMacBook-Pro.local>
2024-02-19 12:31:01 +0900

    div ceil floor move dotfiles

--- num/div_ceil.hpp
+++
@@ -1,3 +0,0 @@
-auto divCeil = []<class T>(T a, T b) -> T {
-    return a / b + (((a ^ b) > 0 and a % b != 0) ? 1 : 0);
-};
--- num/div_floor.hpp
+++
@@ -1,3 +0,0 @@
-auto divFloor = []<class T>(T a, T b) -> T {
-    return a / b - (((a ^ b) < 0 and a % b != 0) ? 1 : 0);
-};
