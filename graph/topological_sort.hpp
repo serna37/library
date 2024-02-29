1831f452fa9c7e00f47db1788994886141e091be
serna37 <serna37@serna37noMacBook-Pro.local>
2024-02-29 09:33:23 +0900

    add topological sort

---
+++ graph/topological_sort.hpp
@@ -0,0 +1,16 @@
+template <typename Graph> vector<int> topological_sort(const Graph &G) {
+    int N = G.size();
+    vector<int> seen(N), sorted;
+    auto dfs = [&](auto f, int v) -> void {
+        seen[v] = 1;
+        for (auto [from, to, cost, i] : G[v]) {
+            if (!seen[to]) f(f, to);
+        }
+        sorted.push_back(v);
+    };
+    for (int i = 0; i < N; i++) {
+        if (!seen[i]) dfs(dfs, i);
+    }
+    reverse(sorted.begin(), sorted.end());
+    return sorted;
+}
