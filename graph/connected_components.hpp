a923bed06612219e621acb2eb84146014b6fd7f6
serna37 <serna37@serna37noMacBook-Pro.local>
2024-03-01 10:44:48 +0900

    add connected components divider

---
+++ graph/connected_components.hpp
@@ -0,0 +1,22 @@
+template <typename Graph>
+vector<vector<int>> connected_components(const Graph &G) {
+    vector<vector<int>> components;
+    vector<int> com;
+    int N = G.size();
+    vector<bool> seen(N, false);
+    auto dfs = [&](auto f, int v) -> void {
+        seen[v] = true;
+        com.push_back(v);
+        for (auto [from, to, cost, id] : G[v]) {
+            if (seen[to]) continue;
+            f(f, to);
+        }
+    };
+    for (int i = 0; i < N; i++) {
+        if (seen[i]) continue;
+        com.clear();
+        dfs(dfs, i);
+        components.push_back(com);
+    }
+    return components;
+}
