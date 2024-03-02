652ecc8feb3beb0dfb9b35b445e30823e9b68dfe
serna37 <serna37@serna37noMacBook-Pro.local>
2024-03-02 13:05:06 +0900

    nCkをn C min(n-k, k)に

--- num/combination.hpp
+++ num/combination.hpp
@@ -5,6 +5,7 @@ using namespace std;
  */
 template <typename T> T nCk(int n, int k) {
     if (k < 0 || n < k) return 0;
+    k = min(n - k, k);
     T ret = 1;
     for (T i = 1; i <= k; ++i) {
         ret *= n--;
