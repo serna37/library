3501e708b5177308c862a605ad48ee436c4adc72
serna37 <serna37@serna37noMacBook-Pro.local>
2024-03-04 21:48:12 +0900

    不要なもの削除

--- mod/comb.hpp
+++ mod/comb.hpp
@@ -3,26 +3,6 @@ using namespace std;
 /**
  * modコンビネーション
  */
-template <typename T> T modpow(T a, T n) {
-    T ans = 1;
-    while (n > 0) {
-        if (n & 1) ans = ans * a % MOD;
-        a = a * a % MOD;
-        n >>= 1ll;
-    }
-    return ans;
-}
-long long modinv(long long a) {
-    return modpow(a, MOD - 2);
-}
-vector<long long> mf;
-long long modfact(int x) {
-    if ((int)mf.size() > x) return mf[x];
-    if (mf.empty()) mf.push_back(1);
-    for (int i = mf.size(); i <= x; i++) mf.push_back(mf.back() * i % MOD);
-    return mf[x];
-}
 long long mod_combination(int n, int k) {
     return modfact(n) * modinv(modfact(k)) % MOD * modinv(modfact(n - k)) % MOD;
 }
-/** --- */
