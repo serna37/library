530b66b76129c5beb1c75635b7513b9798f68009
serna37 <serna37@serna37noMacBook-Pro.local>
2024-03-06 22:57:13 +0900

    modpow T -> long long

--- mod/pow.hpp
+++ mod/pow.hpp
@@ -3,8 +3,8 @@ using namespace std;
 /**
  * mod二分累乗
  */
-template <typename T> T modpow(T a, T n) {
-    T ans = 1;
+long long modpow(long long a, long long n) {
+    long long ans = 1ll;
     while (n > 0) {
         if (n & 1) ans = ans * a % MOD;
         a = a * a % MOD;
