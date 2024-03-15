06d8aef3a88ba80e36a1ec2ce485a05a78c31e03
serna37 <serna37@serna37noMacBook-Pro.local>
2024-03-15 22:12:49 +0900

    modify

--- monoid/set.hpp
+++ monoid/set.hpp
@@ -3,7 +3,7 @@ using namespace std;
 /**
  * 代入モノイド
  */
-template <typename T> struct Madd {
+template <typename T> struct Mset {
     using value_type = T;
     static constexpr T e = inf<T>; // 制約外であること
     static constexpr T op(const T &x, const T &y) noexcept {
