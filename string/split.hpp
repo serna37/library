d7fc38079b076e82381a2068d43d822b82ea12bc
serna37 <serna37@serna37noMacBook-Pro.local>
2024-03-15 11:46:25 +0900

    空文字ありに修正

--- string/split.hpp
+++ string/split.hpp
@@ -5,15 +5,15 @@ using namespace std;
  */
 vector<string> split(const string &S, const char &c) {
     vector<string> res;
-    string t;
+    string t = "";
     for (char v : S) {
         if (v == c) {
-            if (!t.empty()) res.emplace_back(t);
+            res.emplace_back(t);
             t.clear();
         } else {
             t += v;
         }
     }
-    if (!t.empty()) res.emplace_back(t);
+    res.emplace_back(t);
     return res;
 }
