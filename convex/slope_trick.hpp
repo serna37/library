552ad9803ccff86d464e585df54832dd9b1b4bfb
serna37 <serna37@serna37noMacBook-Pro.local>
2024-03-16 15:33:17 +0900

    modify

--- convex/slope_trick.hpp
+++ convex/slope_trick.hpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 // TODO
+template <class T> constexpr T inf = 0;
+template <> constexpr int inf<int> = 1e9;
+template <> constexpr long long inf<long long> = 1e18;
 /**
  * Slope Trick
  */
