2ec3e7e07f40aa7491021e8eead4a28fa28cb693
serna37 <serna37@serna37noMacBook-Pro.local>
2024-03-16 09:24:36 +0900

    いったんいろいろ追加

---
+++ datastruct/segment_tree_beats.hpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+// TODO
+/**
+ * Segment Tree Beats
+ */
---
+++ tree/convert_rooted_tree.hpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+// TODO
+/**
+ * @brief Convert-Rooted-Tree(根付き木に変換)
+ */
+template <typename T>
+Graph<T> convert_rooted_tree(const Graph<T> &g, int r = 0) {
+    int N = (int)g.size();
+    Graph<T> rg(N);
+    vector<int> v(N);
+    v[r] = 1;
+    queue<int> que;
+    que.emplace(r);
+    while (!que.empty()) {
+        auto p = que.front();
+        que.pop();
+        for (auto &to : g[p]) {
+            if (v[to] == 0) {
+                v[to] = 1;
+                que.emplace(to);
+                rg.add_directed_edge(p, to, to.cost);
+            }
+        }
+    }
+    return rg;
+}
